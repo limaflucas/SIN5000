149663436fd59ced9af1b0df0e5e0713178fd6d2
Lucas Lima <lcs.freirelima@gmail.com>
2018-06-11 04:36:39 -0300

    Ajustes

--- revisao_sistematica/artigo/auto/artigo.el
+++ revisao_sistematica/artigo/auto/artigo.el
@@ -4,17 +4,19 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("IEEEtran" "10pt" "journal" "compsoc")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("cite" "nocompress")))
+                     '(("inputenc" "utf8") ("enumitem" "inline") ("cite" "nocompress")))
    (TeX-run-style-hooks
     "latex2e"
     "IEEEtran"
     "IEEEtran10"
     "inputenc"
     "bookmark"
+    "enumitem"
     "cite")
    (LaTeX-add-labels
     "sec:introduction"
     "sec:protocolo"
+    "sec:protocolo:objetivos"
     "sec:protocolo:inclusao"
     "sec:resultados"
     "sec:discussao"
